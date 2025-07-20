cfc9a5045a2807e71733bdb0c18f844a94ca23f0
Justin Hoang <j124.dev@proton.me>
2025-07-20 14:29:13 -0600

    feat(src): done with palindrome optimizations
    had a bit of help!
    it's okay because i'm getting back into it
    just as long as i take good notes and actually understand the problem
    
    Signed-off-by: Justin Hoang <j124.dev@proton.me>

--- src/9.py
+++ src/9.py
@@ -13,10 +13,20 @@ class Solution:
         # 3. the final optimization is to avoid converting number to a string at
         # all. this can be done with modulo and division operators to parse out
         # the left and right numbers at each step
+
+        # edge cases
+        # 1. if x is negative, there is no palindrome possible
+        # 2. if x is a multiple of 10, then the code below will break
+        #    - the reason why is because x_rev will be 0 while x // 10
+        #      continuously pop off the numbers. at the end, we'll claim
+        #      that the numbers are palindromes, which is not true
+        #    - the number 0 however is a palindrome
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+
         x_rev = 0
-        x_copy = x
-        while x > 0:
+        while x > x_rev:
             x_rev = (x_rev * 10) + x % 10
             x //= 10
 
-        return x_copy == x_rev
+        return x == x_rev or x == x_rev // 10
