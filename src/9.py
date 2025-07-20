53ce81d4c91688300e3b5279939a272bbff4bb3b
Justin Hoang <j124.dev@proton.me>
2025-07-20 12:55:33 -0600

    feat(src): slightly optimized palindrome solution with pointers
    Signed-off-by: Justin Hoang <j124.dev@proton.me>

--- src/9.py
+++ src/9.py
@@ -4,4 +4,20 @@ class Solution:
         # the problem is to return whether a number is a boolean or not
         # 1. the naiive (and cheat) solution is to just convert the number to a
         # string (str(x)) and check if the string is equal to its reversed
-        return str(x) == str(x)[::-1]
+        # 2. there is one improvement that could be made to the naiive (1.)
+        # solution.
+        # - we don't need to check if the entire string is equal to its reverse.
+        #   it would be sufficient to use two pointers (left and right)
+        #   iterating from either end that should meet in the middle.
+        #   an optimization like this could cut the time in half
+
+        x = str(x)
+        left, right = 0, len(x) - 1
+
+        while left <= right:
+            if x[left] != x[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
