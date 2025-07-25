068fd5b5fbd0881fb354432f181a3590d9d44723
Justin Hoang <j124.dev@proton.me>
2025-07-25 10:17:53 -0600

    chore(217): worse version of previous solution with counter
    significantly worse memory peformance (90% -> 8%) but similar runtime complexity
    
    Signed-off-by: Justin Hoang <j124.dev@proton.me>

--- src/217.py
+++ src/217.py
@@ -17,18 +17,11 @@ class Solution:
         #      there must be a duplicate
         #    - time: O(nlogn) because of the sorting
 
-        # edge case where the list of numbers is empty. thus it doesn't have any
-        # duplicates trivially
-        if not nums:
-            return False
+        # build the new list of numbers
+        # this should a time complexity O(n) operation
+        counter = defaultdict(int)
+        for num in nums:
+            counter[num] += 1
 
-        sorted_nums = sorted(nums)
-        prev_num = sorted_nums[0]
-        for num in sorted_nums[1:]:
-            if num == prev_num:
-                return True
-
-            # update the previous number
-            prev_num = num
-
-        return False
+        # check if any of the values were not equal to 1
+        return any(val != 1 for val in counter.values())
