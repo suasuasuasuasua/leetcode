36efaa1948e34b252bda6baf81fe88aefdd4de91
Justin Hoang <j124.dev@proton.me>
2025-07-25 10:34:03 -0600

    chore(242): decent improvement with maps
    this is at least time complexity O(n+m) for each loop when we are
    building the dictionary, where n is the size of `s` and m is the size of `t`
    now the time and memory performance is about average (50%)
    
    Signed-off-by: Justin Hoang <j124.dev@proton.me>

--- src/242.py
+++ src/242.py
@@ -5,7 +5,16 @@ class Solution:
         #
         # solutions
         # 1. sort both of the strings and check if they are equal
+        # 2. create two Counter dictionaries to track occurences
+        #    verify that the two counters have the same values for each key
 
-        s = sorted(s)
-        t = sorted(t)
-        return s == t
+        # build the Counter objects
+        s_map = defaultdict(int)
+        for c in s:
+            s_map[c] += 1
+        t_map = defaultdict(int)
+        for c in t:
+            t_map[c] += 1
+
+        # verify that these have the same keys and values
+        return s_map == t_map
