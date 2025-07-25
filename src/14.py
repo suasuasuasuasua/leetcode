7d5a5105a2f303f67ba6f17f3bc9d8c9d52fd02d
Justin Hoang <j124.dev@proton.me>
2025-07-25 13:01:08 -0600

    feat(14): done with common prefix problem
    100% runtime and 96% memory here wow
    didn't need to use a trie at all
    it was just a O(n) go through all the strings at the same time problem
    
    Signed-off-by: Justin Hoang <j124.dev@proton.me>

--- src/14.py
+++ src/14.py
@@ -13,6 +13,28 @@ class Solution:
         # are the same value until they aren't
         # - i imagine that this is the _very_ naiive solution but it may be
         #   worth doing to see what can be improved
+
+        # sanity check if
+        # 1. the list is empty
+        # 2. any of the strings in the list are empty
+        if not strs or any(len(s) == 0 for s in strs):
+            return ""
+
+        # we should only check up to the smallest string in the list
+        min_n = min(len(s) for s in strs)
         result = ""
+        # loop over 0 -> min_n
+        for i in range(min_n):
+            # grab the current characters from all of the strings at the same
+            # time using i
+            current_chars = set(s[i] for s in strs)
+
+            # ensure that the length is 1, meaning that all the elements at
+            # index i are the same, meaning that they share a prefix still
+            if len(current_chars) == 1:
+                # add it to the result string
+                result += list(current_chars)[0]
+            else:
+                break
 
         return result
