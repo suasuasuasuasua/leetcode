40a3d7277af19a5c62fbdee756fdb020b82aece2
Justin Hoang <j124.dev@proton.me>
2025-07-25 10:25:34 -0600

    chore(217): skip using dictionary, use a set instead
    if we're not interested in the count, then don't track that
    this removes tons of overhead of tracking key/value pairs
    
    Signed-off-by: Justin Hoang <j124.dev@proton.me>

--- src/217.py
+++ src/217.py
@@ -6,16 +6,16 @@ class Solution:
         # problem: given a list of numbers, check if the list contains any
         # duplicates
 
-        # use a dictionary to track whether we've seen an element or not
-        seen = defaultdict(bool)
+        # use a set to track whether we've seen an element or not
+        seen = set()
         for num in nums:
-            # if the number is already in the dictionary, then we've already
+            # if the number is already in the set, then we've already
             # seen it! there is a duplicate!
             if num in seen:
                 return True
 
             # mark the number as seen
-            seen[num] = True
+            seen.add(num)
 
         # if we don't otherwise break early in the loop, then there must not be
         # any duplicates
