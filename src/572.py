8ec10366499392cfd5cce288fb7099de3ef7bbaa
Justin Hoang <j124.dev@proton.me>
2025-07-27 14:01:11 -0600

    feat(572): initial solution for subtree of another tree
    i think it may be a bit brute forced
    but the memory usage is pretty decent
    
    there may be a way to "cache" the result

---
+++ src/572.py
@@ -0,0 +1,43 @@
+# https://leetcode.com/problems/subtree-of-another-tree/
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        # problem
+        # given a root tree, check if a subtree is present in the root tree
+        #
+        # nuance
+        # a root tree can be a subroot of itself
+        #
+        # solution
+        # perform a depth first search on the root tree
+        # check if the tree is equal at each node (see problem 100.)
+
+        def is_equal(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+            match p, q:
+                case None, None:
+                    return True
+                case (TreeNode(), None) | (None, TreeNode()):
+                    return False
+                case _:
+                    return (
+                        p.val == q.val
+                        and is_equal(p.left, q.left)
+                        and is_equal(p.right, q.right)
+                    )
+
+        def dfs(p: Optional[TreeNode]) -> bool:
+            # base case where we hit an empty node
+            # trivially, this root does not contain the subroot
+            if not p:
+                return False
+
+            # otherwise, check if the current root is equal to the subroot
+            # also, we OR the dfs on the left and right children
+            return is_equal(p, subRoot) or dfs(p.left) or dfs(p.right)
+
+        return dfs(root)
