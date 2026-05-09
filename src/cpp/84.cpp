5c480311372ae8cd5a5e2a9d54cc48ab7185b135
Justin Hoang <justinhoang@sua.dev>
2026-05-09 16:33:53 -0600

    feat: use better solution with actual monotonic ascending stack
    the trick is very tricky

--- src/cpp/84.cpp
+++ src/cpp/84.cpp
@@ -12,32 +12,45 @@ public:
 
     // use a monotonically ascending stack to find the largest left and
     // rightmost bounds possible
-    std::stack<int> s;
+    //
+    // a monotonically ascending stack looks like in histogram form
+    //
+    //          []    <- can no longer be extended right (width=4-3, area=4*1)
+    //       [] []    <- same here (width=4-2, area=3*2)
+    //    [] [] [] []
+    // [] [] [] [] []
+    // 0  1  2  3  4
+    std::stack<std::pair<std::size_t, int>> s;
 
     int area = 0;
-    for (int height : heights) {
-      // use the height of the bar as the area
-      area = std::max(area, height);
+    for (std::size_t i = 0; i < heights.size(); i++) {
+      std::size_t start = i;
+      int height = heights.at(i);
 
-      // create temporary stack to iterate through
-      std::stack<int> temp_s(s);
-
-      // look left as far back as we can to construct the width
-      int width = 1;
-      int temp_height = height; // init as current height
-      while (!temp_s.empty()) {
-        int prev_height = temp_s.top();
-        // use smaller and smaller heights if encountered
-        if (prev_height < temp_height) {
-          temp_height = prev_height; // attempt constructing off previous height
-        }
-        temp_s.pop();
-        width++;
-        // attempt to get a higher area
-        area = std::max(area, temp_height * width);
+      // repeatedly pop off the stack while the current element is the smallest
+      // this means that the height can no longer be extended to the right, and
+      // it should just be considered in isolation
+      while (!s.empty() && height < s.top().second) {
+        int width = i - s.top().first;
+        area = std::max(area, s.top().second * width);
+        // reset the new height's starting index to this (popped) height's index
+        // NOTE: can't just increment an offset by -1 because the popped heights
+        // are not necessarily in order
+        start = s.top().first;
+        s.pop();
       }
 
-      s.push(height);
+      s.push({start, height});
+    }
+
+    // these heights must have extended to the end of the histogram.
+    // the width is the difference between the length of the numbers array and
+    // the index, while the height is simply the height value
+    while (!s.empty()) {
+      auto [idx, height] = s.top();
+      auto width = heights.size() - idx;
+      area = std::max(area, static_cast<int>(height * width));
+      s.pop();
     }
 
     return area;
@@ -51,6 +64,7 @@ int main(int argc, char *argv[]) {
   std::vector<std::pair<inputs_t, outputs_t>> tests = {
       {{7, 1, 7, 2, 2, 4}, 8},
       {{2, 1, 5, 6, 2, 3}, 10},
+      {{3, 6, 5, 7, 4, 8, 1, 0}, 20},
   };
 
   for (auto &[nums, expected] : tests) {
