aeb110c9c6f5d534e67aa6626a2866c0e3252d15
Justin Hoang <justinhoang@sua.dev>
2026-07-10 17:48:01 -0600

    fix(15): use a seen set to track fixed nums

--- src/python/15.py
+++ src/python/15.py
@@ -6,13 +6,16 @@ class Solution:
         #   return all the triplets where i != j != j
         #          and nums[i] + nums[j] + nums[k] == 0
 
-        result = set()
+        result = list()
+        seen = set()
 
         # first sort the array
         nums = sorted(nums)
 
         # second, run the fixed looked up algorithm
         for fixed, fixed_num in enumerate(nums):
+            if fixed_num in seen:
+                continue
             # a + b + c = 0
             # b + c = -a
             target = -fixed_num
@@ -22,13 +25,22 @@ class Solution:
             while left < right:
                 curr_sum = nums[left] + nums[right]
                 if curr_sum == target:
-                    result.add(tuple(sorted([fixed_num, nums[left], nums[right]])))
+                    result.append([fixed_num, nums[left], nums[right]])
                     left += 1
                     right -= 1
-                    continue
+                    # the array is sorted -- skip over duplicate numbers
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    # the array is sorted -- skip over duplicate numbers
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
                 elif curr_sum < target:
                     left += 1
                 else:
                     right -= 1
 
-        return [list(r) for r in result]
+            # the inner loop tracks all of the triplets
+            # don't even try for numbers that we've already seen
+            seen.add(fixed_num)
+
+        return result
